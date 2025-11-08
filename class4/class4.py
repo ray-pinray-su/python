2465e69473b01717575fef5781b3a91fe9c2c1bf
ray-pinray-su <ray.pinray.su@gmail.com>
2025-11-08 15:07:54 +0800

    新增成績評分功能，根據輸入的成績輸出對應的評分

--- class4/class4.py
+++ class4/class4.py
@@ -1,1 +1,11 @@
-
+d = int(input("請輸入成績:"))
+if d > 89:
+    print("A")
+elif d > 79 and d < 90:
+    print("B")
+elif d > 69 and d < 80:
+    print("C")
+elif d > 59 and d < 70:
+    print("D")
+else:
+    print("E")
