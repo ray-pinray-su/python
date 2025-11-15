da99a755fa3da77760a8364b2dd1ec28d1644e4d
ray-pinray-su <ray.pinray.su@gmail.com>
2025-11-15 17:06:15 +0800

    修正繪圖邏輯，調整迴圈次數以改善箭頭繪製效果

--- class5/gsdfuthyitior.py
+++ class5/gsdfuthyitior.py
@@ -1,11 +1,13 @@
 import turtle as t
 
+t.penup()
 t.speed(0)
 t.forward(-500)
 t.left(90)
 t.forward(300)
 t.right(90)
-for i in range(1000):
+t.pendown()
+for i in range(250):
     t.forward(1000)
     t.right(90)
     t.forward(1)
