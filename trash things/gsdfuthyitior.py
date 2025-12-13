f8453e8a082a0aa4ff72259138f89d37f95dcf9f
ray-pinray-su <ray.pinray.su@gmail.com>
2025-12-13 14:38:39 +0800

    調整 turtle 繪圖參數，增加繪圖範圍與細節；新增空白檔案以便後續擴展

--- trash things/gsdfuthyitior.py
+++ trash things/gsdfuthyitior.py
@@ -2,17 +2,17 @@ import turtle as t
 
 t.penup()
 t.speed(0)
-t.forward(-500)
+t.forward(-700)
 t.left(90)
-t.forward(300)
+t.forward(350)
 t.right(90)
 t.pendown()
-for i in range(250):
-    t.forward(1000)
+for i in range(400):
+    t.forward(2000)
     t.right(90)
     t.forward(1)
     t.right(90)
-    t.forward(1000)
+    t.forward(2000)
     t.left(90)
     t.forward(1)
     t.left(90)
