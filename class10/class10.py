60abc3c37b3e8398505c8469e3e14206f300e3d4
ray-pinray-su <ray.pinray.su@gmail.com>
2025-12-20 16:44:52 +0800

    新增列表操作功能，增強使用者互動體驗；新增資料修改與刪除選項，提升程式靈活性

--- class10/class10.py
+++ class10/class10.py
@@ -46,25 +46,75 @@ d = ["1.新增", "2.刪除", "3.修改", "4.離開"]
 while True:
     print(l)
     for i in d:
+
         print(i)
     x = input("請輸入指令:")
     if x == "1":
+
         n = input("請輸入新增的資料:")
-        l.append(n)
+        z = input("請問要放在最後面(a)還是指定位置(b)?(輸入a或b):")
+        if z == "b":
+            pos = int(input("請輸入位置(從0開始):"))
+            l.insert(pos, n)
+        else:
+            l.append(n)
     elif x == "2":
-        n = input("請輸入要刪除的資料:")
-        if n in l:
-            l.remove(n)
+
+        g = input("請問要刪除第一筆(a)還是指定(b)?(輸入a或b):")
+        if g == "b":
+            pos = int(input("請輸入位置(從0開始):"))
+            if pos >= 0 and pos < len(l):
+                l.pop(pos)
+            else:
+                print("位置不正確")
+        if g == "a":
+            if n in l:
+                l.remove(l[0])
         else:
             print("沒有這筆資料")
     elif x == "3":
-        n = input("請輸入要修改的資料:")
-        if n in l:
-            m = input("請輸入新的資料:")
-            index = l.index(n)
-            l[index] = m
-        else:
-            print("沒有這筆資料")
+        e = input("請問要修改第一筆(a)還是指定(b)?(輸入a或b):")
+        if e == "a":
+            l[0] = input("請輸入新的資料:")
+        if e == "b":
+            n = input("請輸入要修改的資料:")
+            if n not in l:
+                print("沒有這筆資料可以修改")
+                continue
+            else:
+                m = input("請輸入新的資料:")
+                index = l.index(n)
+                l[index] = m
+
     elif x == "4":
         print("離開程式")
         break
+
+
+# enumerate:同時取得List的index跟內容
+# 使用enumerate可以在迴圈中同時取得List的index跟內容
+l = ["apple", "banana", "cherry"]
+for index, value in enumerate(l):
+    print(index, value)
+    # 0 apple
+    # 1 banana
+    # 2 cherry
+
+
+# `in`可以用在很多地方,像是if條件判斷
+# 判斷某個元素是否在List當中
+l = ["apple", "banana", "cherry"]
+if "banana" in l:
+    print("有香蕉")
+else:
+    print("沒有香蕉")
+
+# in在for迴圈中代表迴圈變數會依序取得右邊範圍中的每一個元素
+l = ["apple", "banana", "cherry"]
+for fruit in l:
+    print("我喜歡吃", fruit)
+
+# in還可以用在字串中,檢查某個字串是否在字串中
+s = "hello world"
+if "world" in s:
+    print("有world這個字串")
