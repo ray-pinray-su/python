d0a1b988379c9212f3bc7f02d3d6822c5cc08989
ray-pinray-su <ray.pinray.su@gmail.com>
2026-01-17 15:08:45 +0800

    新增 class14.py 檔案，實作水果價格管理功能，增強互動性與使用體驗

---
+++ class14/class14.py
@@ -0,0 +1,42 @@
+g = {"蘋果": 25, "香蕉": 30, "橘子": 40}
+l = ["新增水果價格", "修改價格", "刪除", "離開"]
+while True:
+    print("目前價格:")
+    for k, v in g.items():
+        print(f"{k}:{v}")
+    for i, f in enumerate(l):
+        print(f"{i+1}.{f}")
+    t = int(input("請選1-4:"))
+    c = t - 1
+
+    def add():
+        n = input("請輸入水果名稱:")
+        p = int(input("請輸入價格:"))
+        g[n] = p
+
+    def edit():
+        n = input("請輸入要修改的水果名稱:")
+        if n in g:
+            p = int(input("請輸入新的價格:"))
+            g[n] = p
+        else:
+            print("沒有這水果")
+
+    def delete():
+        n = input("請輸入要刪除的水果名稱:")
+        if n in g:
+            g.pop(n)
+        else:
+            print("沒這水果")
+
+    if c == 0:
+        add()
+    elif c == 1:
+        edit()
+    elif c == 2:
+        delete()
+
+    elif c == 3:
+        print("離開程式")
+        break
+print("=" * 100)
